af55b09b88f4c0ae087e34958e7911241d0b8564
MohammadKevin <kvn4.200581@gmail.com>
2026-08-22 19:40:23 +0700

    fix(auth): add personal_access_tokens migration and optimize register execution speed

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -63,7 +63,7 @@ class AuthController extends Controller
         $user = User::create([
             'name'     => $request->name,
             'email'    => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => $request->password,
             'role'     => 'customer',
         ]);
 
