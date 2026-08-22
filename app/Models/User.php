751dd6833805b203afc4eaedbc49f394f75e30bf
MohammadKevin <kvn4.200581@gmail.com>
2026-08-23 01:20:27 +0700

    fix(user): add missing HasApiTokens import in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, HasObfuscatedId;
