526bd0096d52e2dfdc0a3e1b93be263ab83eba17
MohammadKevin <kvn4.200581@gmail.com>
2026-08-24 22:39:42 +0700

    fix(export): escape xml tags from blade component parser in excel template

--- resources/views/super_admin/reports/excel_template.blade.php
+++ resources/views/super_admin/reports/excel_template.blade.php
@@ -1,20 +1,7 @@
 <html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <!--[if gte mso 9]>
-    <xml>
-        <x:ExcelWorkbook>
-            <x:ExcelWorksheets>
-                <x:ExcelWorksheet>
-                    <x:Name>Laporan Keuangan</x:Name>
-                    <x:WorksheetOptions>
-                        <x:DisplayGridlines/>
-                    </x:WorksheetOptions>
-                </x:ExcelWorksheet>
-            </x:ExcelWorksheets>
-        </x:ExcelWorkbook>
-    </xml>
-    <![endif]-->
+    {!! '<!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>Laporan Keuangan</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]-->' !!}
     <style>
         body, table {
             font-family: Arial, Helvetica, sans-serif;
